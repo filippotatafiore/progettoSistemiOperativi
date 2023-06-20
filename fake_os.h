114620c9cc89cf090a8e75059c63b264ddf0bf33
Filippo <filippotatafiore.01@gmail.com>
2023-06-20 19:29:29 +0200

    Aggiunta fake_os.h. Aggiunto i campi durata_burst e prediction_burst di FakePCB (per la prediction della durata dei cpu burts). Modificato il campo running di FakeOS, impostato come una lista (per implementare multiple cpus).

---
+++ fake_os.h
@@ -0,0 +1,38 @@
+#include "fake_process.h"
+#include "linked_list.h"
+#pragma once
+
+
+typedef struct {  //PCB
+  ListItem list;  //Un PCB farà parte di una lista (ready, waiting, ...)
+  int pid;  //pid del processo associato
+  ListHead events;  //lista di eventi (CPU burst e I/O burst) del processo
+  int durata_burst;  //durata del cpu burst eseguito
+  int prediction_burst;  //predizione del cpu burst
+} FakePCB;
+
+
+struct FakeOS;
+//definisco ScheduleFn, puntatore a una funzione void che prende gli argomenti os e args
+typedef void (*ScheduleFn)(struct FakeOS* os, void* args);
+
+
+typedef struct FakeOS{
+  ListHead running;  //lista dei processi in esecuzione (lista di FakePCB)
+  ListHead ready;  //lista ready (lista di FakePCB)
+  ListHead waiting;  //lista waiting (lista di FakePCB)
+  int timer;  //intero che usiamo per contare
+  ScheduleFn schedule_fn;  //puntatore a una funzione di scheduling
+  void* schedule_args;  //argomenti da passare alla funzione di scheduling
+  ListHead processes;  //lista di tutti i processi (lista di FakeProcess)
+} FakeOS;
+
+//costruttore, inizializza i campi os
+void FakeOS_init(FakeOS* os);
+
+//fa "girare la giostra"
+void FakeOS_simStep(FakeOS* os);
+
+//libera memoria del sistema
+void FakeOS_destroy(FakeOS* os);
+
